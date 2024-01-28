0cec4b5ada2ed667c392cd988e0e7ffdeba43dc8
n.zare <n.zare@behsacorp.com>
2024-01-28 19:53:52 +0330

    first commit

---
+++ src/main/java/org/behsa/posinterface/PosinterfaceApplication.java
@@ -0,0 +1,13 @@
+package org.behsa.posinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosinterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosinterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/behsa/posinterface/PosinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.behsa.posinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosinterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
